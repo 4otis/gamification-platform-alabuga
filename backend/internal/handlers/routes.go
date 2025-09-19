651ed777a8f2cd48e0da7fe4e4993702abc25d4c
Khariton <khariton.crp@mail.ru>
2025-09-19 19:36:34 +0300

    hr entity was added. student entity was fixed. New handlers, servicies and repos were added.

--- backend/internal/handlers/routes.go
+++ backend/internal/handlers/routes.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/gorm"
+)
+
+func SetupRoutes(g *gin.Engine, db *gorm.DB) {
+	hrRepo := repository.NewHRRepository(db)
+	studentRepo := repository.NewStudentRepository(db)
+	courseRepo := repository.NewAssetRepository(db)
+	missionRepo := repository.NewMarketRepository(db)
+
+	studentService := services.NewSessionService(sessionRepo, playerRepo)
+	hrService := services.NewGameService(sessionRepo, playerRepo)
+
+	// hrHandler := NewSessionHandler()
+	// studentHandler := NewGameHandler(gameService)
+
+	g.StaticFile("/", "./index.html")
+	g.StaticFile("/index.html", "./index.html")
+
+	g.POST("/courses", courseHandler.GetAllCourses)
+
+	g.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
--- backend/internal/repository/student.go
+++ backend/internal/repository/student.go
@@ -1,1 +1,35 @@
 package repository
+
+import (
+	"github.com/4otis/gamification-platform-alabuga/internal/models"
+	"gorm.io/gorm"
+)
+
+type StudentRepository struct {
+	db *gorm.DB
+}
+
+func NewStudentRepository(db *gorm.DB) *StudentRepository {
+	return &StudentRepository{db: db}
+}
+
+func (r StudentRepository) Create(student *models.Student) error {
+	return r.db.Create(student).Error
+}
+
+func (r StudentRepository) Read(login string) (*models.Student, error) {
+	var student models.Student
+	err := r.db.Where("login = ?", login).First(&student).Error
+	if err != nil {
+		return nil, err
+	}
+	return &student, nil
+}
+
+func (r StudentRepository) UpdateFields(id uint, updates map[string]interface{}) error {
+	return r.db.Model(&models.Student{}).Where("id = ?", id).Updates(updates).Error
+}
+
+func (r StudentRepository) Delete(id uint) error {
+	return r.db.Delete(&models.Student{}, id).Error
+}
