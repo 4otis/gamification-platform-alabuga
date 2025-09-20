f76763381a6363e37ec824be36590c6113cf2d0a
Khariton <khariton.crp@mail.ru>
2025-09-20 16:08:06 +0300

    Some .sql-scripts were upgraded. delete.sql has been added.

--- backend/internal/migrations/sql/constraints.sql
+++ backend/internal/migrations/sql/constraints.sql
@@ -1,47 +1,47 @@
--- Ограничения таблицы Student_Rank
-ALTER TABLE Student_Rank
+-- Ограничения таблицы student_rank
+ALTER TABLE student_rank
 ALTER COLUMN name SET NOT NULL,
-ALTER COLUMN minExp SET NOT NULL;
+ALTER COLUMN min_exp SET NOT NULL;
 
-ALTER TABLE Student_Rank
-ALTER COLUMN minExp SET DEFAULT 0;
+ALTER TABLE student_rank
+ALTER COLUMN min_exp SET DEFAULT 0;
 
-ALTER TABLE Student_Rank
-ADD CONSTRAINT check_if_min_exp_is_not_negative CHECK (minExp >= 0);
+ALTER TABLE student_rank
+ADD CONSTRAINT check_if_min_exp_is_not_negative CHECK (min_exp >= 0);
 
--- Ограничения таблицы HR
-ALTER TABLE HR
+-- Ограничения таблицы hr
+ALTER TABLE hr
 ALTER COLUMN login SET NOT NULL,
 ALTER COLUMN password SET NOT NULL,
 ALTER COLUMN name SET NOT NULL,
 ALTER COLUMN surname SET NOT NULL;
 
-ALTER TABLE HR
+ALTER TABLE hr
 ADD CONSTRAINT unique_hr_login UNIQUE (login);
 
--- Ограничения таблицы Mission_Type
-ALTER TABLE Mission_Type
+-- Ограничения таблицы mission_type
+ALTER TABLE mission_type
 ALTER COLUMN name SET NOT NULL;
 
-ALTER TABLE Mission_Type
+ALTER TABLE mission_type
 ADD CONSTRAINT unique_mission_type_name UNIQUE (name);
 
--- Ограничения таблицы Skill
-ALTER TABLE Skill
+-- Ограничения таблицы skill
+ALTER TABLE skill
 ALTER COLUMN name SET NOT NULL;
 
-ALTER TABLE Skill
+ALTER TABLE skill
 ADD CONSTRAINT unique_skill_name UNIQUE (name);
 
--- Ограничения таблицы Rarity
-ALTER TABLE Rarity
+-- Ограничения таблицы rarity
+ALTER TABLE rarity
 ALTER COLUMN name SET NOT NULL;
 
-ALTER TABLE Rarity
+ALTER TABLE rarity
 ADD CONSTRAINT unique_rarity_name UNIQUE (name);
 
--- Ограничения таблицы Student
-ALTER TABLE Student
+-- Ограничения таблицы student
+ALTER TABLE student
 ALTER COLUMN login SET NOT NULL,
 ALTER COLUMN password SET NOT NULL,
 ALTER COLUMN name SET NOT NULL,
@@ -49,59 +49,59 @@ ALTER COLUMN surname SET NOT NULL,
 ALTER COLUMN exp SET NOT NULL,
 ALTER COLUMN mana SET NOT NULL;
 
-ALTER TABLE Student
+ALTER TABLE student
 ALTER COLUMN exp SET DEFAULT 0,
 ALTER COLUMN mana SET DEFAULT 0;
 
-ALTER TABLE Student
+ALTER TABLE student
 ADD CONSTRAINT unique_student_login UNIQUE (login);
 
-ALTER TABLE Student
+ALTER TABLE student
 ADD CONSTRAINT check_if_exp_is_not_negative CHECK (exp >= 0),
 ADD CONSTRAINT check_if_mana_is_not_negative CHECK (mana >= 0);
 
--- Ограничения таблицы Artifact
-ALTER TABLE Artifact
+-- Ограничения таблицы artifact
+ALTER TABLE artifact
 ALTER COLUMN title SET NOT NULL,
-ALTER COLUMN filePath SET NOT NULL;
+ALTER COLUMN file_path SET NOT NULL;
 
-ALTER TABLE Artifact
+ALTER TABLE artifact
 ADD CONSTRAINT unique_artifact_title UNIQUE (title);
 
--- Ограничения таблицы Course
-ALTER TABLE Course
+-- Ограничения таблицы course
+ALTER TABLE course
 ALTER COLUMN title SET NOT NULL;
 
-ALTER TABLE Course
+ALTER TABLE course
 ADD CONSTRAINT unique_course_title UNIQUE (title);
 
--- Ограничения таблицы Mission
-ALTER TABLE Mission
+-- Ограничения таблицы mission
+ALTER TABLE mission
 ALTER COLUMN title SET NOT NULL,
-ALTER COLUMN manaReward SET NOT NULL,
-ALTER COLUMN expReward SET NOT NULL,
-ALTER COLUMN nodeLvl SET NOT NULL;
+ALTER COLUMN mana_reward SET NOT NULL,
+ALTER COLUMN exp_reward SET NOT NULL,
+ALTER COLUMN node_lvl SET NOT NULL;
 
-ALTER TABLE Mission
-ADD CONSTRAINT check_if_exp_reward_is_not_negative CHECK (expReward >= 0),
-ADD CONSTRAINT check_if_mana_reward_is_not_negative CHECK (manaReward >= 0);
+ALTER TABLE mission
+ADD CONSTRAINT check_if_exp_reward_is_not_negative CHECK (exp_reward >= 0),
+ADD CONSTRAINT check_if_mana_reward_is_not_negative CHECK (mana_reward >= 0);
 
--- Ограничения таблицы Students_Skills
-ALTER TABLE Students_Skills
+-- Ограничения таблицы students_skills
+ALTER TABLE students_skills
 ALTER COLUMN score SET NOT NULL;
 
-ALTER TABLE Students_Skills
+ALTER TABLE students_skills
 ALTER COLUMN score SET DEFAULT 0.0;
 
-ALTER TABLE Students_Skills
+ALTER TABLE students_skills
 ADD CONSTRAINT check_if_score_is_not_negative CHECK (score >= 0.0);
 
--- Ограничения таблицы Mission_Skills
-ALTER TABLE Mission_Skills
-ALTER COLUMN scoreReward SET NOT NULL;
+-- Ограничения таблицы missions_skills
+ALTER TABLE missions_skills
+ALTER COLUMN score_reward SET NOT NULL;
 
-ALTER TABLE Mission_Skills
-ALTER COLUMN scoreReward SET DEFAULT 0.0;
+ALTER TABLE missions_skills
+ALTER COLUMN score_reward SET DEFAULT 0.0;
 
-ALTER TABLE Mission_Skills
-ADD CONSTRAINT check_if_score_reward_is_not_negative CHECK (scoreReward >= 0.0);
+ALTER TABLE missions_skills
+ADD CONSTRAINT check_if_score_reward_is_not_negative CHECK (score_reward >= 0.0);
